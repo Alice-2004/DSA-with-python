34ecbcba3296f23af7bd0e4cf66b358508b8cfd2
Alice-2004 <aliceverma97@gmail.com>
2025-05-27 01:14:40 +0530

    added problem solution

---
+++ Tree/100_leetcode.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def isSameTree(self, p, q):
+        if not p and not q:
+            return True
+        if not p or not q:
+            return False
+        if p.val != q.val:
+            return False
+        left_check=self.isSameTree(p.left, q.left)
+        right_check=self.isSameTree(p.right, q.right)
+        if left_check and right_check:
+            return True
+        else:
+            return False 
+
+        """
+        :type p: Optional[TreeNode]
+        :type q: Optional[TreeNode]
+        :rtype: bool
+        """
+        #TC -O(n)
+        #SC -O(h)
